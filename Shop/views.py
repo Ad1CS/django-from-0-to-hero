2d4922594d3bf3aec30c295003495acaf1bf80dc
ad1cs <forestmonkeys73@gmail.com>
2025-08-22 14:06:14 +0300

    added main-page and about

--- Shop/views.py
+++ Shop/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def mainpage(request):
+    return HttpResponse('mainpage')
+
+def about(request):
+    return HttpResponse('about')
--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from Shop.views import mainpage, about
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',mainpage),
+    path('about/', about)
 ]
