9ce42bd21107489b4d5cd9cfde729be6bbf9a293
ad1cs <forestmonkeys73@gmail.com>
2025-08-29 16:30:44 +0300

    in normal python we take an element by [number] in django we use ccommand.number/key

--- Shop/views.py
+++ Shop/views.py
@@ -1,8 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+class MyClass:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
 
 def mainpage(request):
-    return render(request, 'index.html')
+    data = {
+        'title': 'Главная страница',
+        'menu': menu,
+        'float': 28.56,
+        'lst': [1, 2, 'abc', True],
+        'set': {1, 1, 2, 3, 2, 5},
+        'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
+        'obj': MyClass(10, 20),
+    }
+    return render(request, 'index.html', data)
 
 def about(request):
     return render(request, 'about.html')
